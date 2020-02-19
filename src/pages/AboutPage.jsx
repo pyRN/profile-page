c4243e53586c5452db3ba5c79a65cb740f068f13
Jacob <jacobmayeux@ibm.com>
2020-02-19 11:57:18 -0600

    Added img to about page, added contact form to contact page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,8 +5,17 @@ class AboutPage extends Component {
     render() { 
         return ( 
             <div className="pageComponentStyle">
-                <h1>About</h1>
-                
+                <h2>About</h2>
+                <div className="row">
+                    <div className="col">
+                        <img src={require('../media/self_img.jpeg')} className="aboutImgStyle rounded-circle" alt="self_img.jpeg"/>
+                    </div>
+                    <div className="col">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, quibusdam? Libero reprehenderit sint vero, minima voluptatem eligendi sit neque nostrum nulla maiores adipisci atque? Nostrum veniam voluptates reiciendis dolore? Rem.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt sunt sequi, animi atque, porro ex ipsum possimus voluptas maiores reiciendis aliquam a iste architecto placeat velit. Accusamus tenetur quia incidunt?
+                        </p>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,10 +5,33 @@ class ContactPage extends Component {
     render() { 
         return ( 
             <div className="pageComponentStyle">
-                <h2>Contact</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente necessitatibus molestiae ipsum veritatis excepturi odio exercitationem eligendi, explicabo voluptatibus perspiciatis consequuntur mollitia doloremque vel totam recusandae dolores dicta. Dicta, voluptatum!
-                    
-                </p>
+                <div className="card pageComponentStyle">
+                    <h5 className="card-header iwhite-text text-left py-1">
+                        <strong>Contact Me</strong>
+                    </h5>
+
+                    <div className="card-body ">
+
+                        <form className="text-center" action="#!">
+
+                            <div className="md-form mb-3">
+                                <input type="text" id="materialContactFormName" placeholder="Name" className="form-control"/>
+                            </div>
+
+                            <div className="md-form mb-3">
+                                <input type="email" id="materialContactFormEmail" placeholder="Email" className="form-control"/>
+                            </div>
+
+                            <div className="md-form mb-3">
+                                <textarea id="materialContactFormMessage" placeholder="Message" className="form-control md-textarea" rows="3"></textarea>
+                            </div>
+
+                            <button className="btn btn-outline-info btn-rounded btn-block z-depth-0 my-4 waves-effect" type="submit">Send</button>
+
+                        </form>
+
+                    </div>
+                </div>
             </div>
         );
     }
